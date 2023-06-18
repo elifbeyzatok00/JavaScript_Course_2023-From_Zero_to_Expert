74d6db0a8df1a120ac97fb5843137b9a99e58bfd
Elif Beyza Tok <102792446+elifbeyzatok00@users.noreply.github.com>
2023-06-18 14:12:38 +0300

    Delete 01-Fundamentals-Part-1 directory

--- 01-Fundamentals-Part-1/Lesson Codes/README.md
+++
@@ -1,1 +0,0 @@
-### In this folder,I am adding files while I am coding what teacher codes in the lessons
--- 01-Fundamentals-Part-1/Practice Assignments/script.js
+++
@@ -1,8 +0,0 @@
-//  LECTURE: Values and Variables
-let country = "Turkiye";
-let continent = "both Europe and Asia";
-let population = "more than 84 million";
-
-console.log(
-  `My country is ${country}. Turkiye is located in ${continent}. My country's population is ${population} in 2023.`
-);
