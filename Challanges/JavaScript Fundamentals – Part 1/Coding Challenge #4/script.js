15f4e524a4c84066d3c6eaf55c6a86a28716174d
Elif Beyza Tok <102792446+elifbeyzatok00@users.noreply.github.com>
2023-06-23 20:09:13 +0300

    Add files via upload

---
+++ Challanges/JavaScript Fundamentals – Part 1/Coding Challenge #4/script.js
@@ -0,0 +1,8 @@
+/*
+Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+*/
+let bill = 275;
+let tip = 50 <= bill < 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
