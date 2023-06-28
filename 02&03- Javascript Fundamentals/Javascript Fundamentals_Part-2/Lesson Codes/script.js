8f8f80019b2fcadf61bf7e927d39e99bb1d1db01
Elif Beyza Tok <102792446+elifbeyzatok00@users.noreply.github.com>
2023-06-28 17:58:22 +0300

    Update script.js

--- 02&03- Javascript Fundamentals/Javascript Fundamentals_Part-2/Lesson Codes/script.js
+++ 02&03- Javascript Fundamentals/Javascript Fundamentals_Part-2/Lesson Codes/script.js
@@ -53,3 +53,34 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1991);
 console.log(age2);
+
+console.log("---------- 35. Arrow Functions -----------");
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //   return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
+
+console.log("---------- 36. Functions Calling Other Functions -----------");
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+
