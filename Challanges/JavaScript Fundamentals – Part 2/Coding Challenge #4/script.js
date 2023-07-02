56627aded24c6aa5497c32d4da6492251851eed0
Elif Beyza Tok <102792446+elifbeyzatok00@users.noreply.github.com>
2023-07-02 16:57:01 +0300

    Add files via upload

---
+++ Challanges/JavaScript Fundamentals – Part 2/Coding Challenge #4/script.js
@@ -0,0 +1,19 @@
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+*/
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
